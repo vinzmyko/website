14165c6aead0ba4d53344fae6c057f9a8fbd6203
Vinz <vinzmykodelrosario@gmail.com>
2024-09-23 21:59:36 +0100

    build(styles): integrate Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Pages/**/*.{razor,html,cshtml}',
+    './Components/**/*.{razor,html,cshtml}',
+    './wwwroot/**/*.html',
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
