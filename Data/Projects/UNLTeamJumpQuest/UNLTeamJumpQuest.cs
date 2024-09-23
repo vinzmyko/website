c1a865af4328b10b4d5011b0ca2b134c5375df86
Vinz <vinzmykodelrosario@gmail.com>
2024-09-23 21:44:40 +0100

    content(projects): add UNL Team Jump Quest project

---
+++ Data/Projects/UNLTeamJumpQuest/UNLTeamJumpQuest.cs
@@ -0,0 +1,29 @@
+﻿using PersonalWebsite.Models;
+
+namespace PersonalWebsite.Data.Projects;
+
+public class UNLTeamJumpQuest : Project
+{
+    public UNLTeamJumpQuest()
+    {
+        Name = "UNL Team Jump Quest";
+        DateStarted = new DateTime(2024, 8, 3); 
+        DateFinished = new DateTime(2024, 8, 30);
+        PrimaryCategory = PrimaryCategory.GameDevelopment;
+        SecondaryCategories = new List<SecondaryCategory> 
+        { 
+            SecondaryCategory.Godot,
+        };
+        Technologies = new List<Technology> 
+        { 
+            Technology.CSharp,
+            Technology.Godot
+        };
+        Attributes = new List<ProjectAttribute> 
+        { 
+            ProjectAttribute.TeamProject,
+            ProjectAttribute.ClientWork
+        };
+        Scale = ProjectScale.Published;
+    }
+}
